2430f509b71609128311bc17a6435dd45d09faf5
CJBJ1 <cbjx17@naver.com>
2020-04-12 00:08:43 +0900

    로그인 액티비티 및 레이아웃 추가

---
+++ app/src/main/java/com/example/usans/LoginActivity.java
@@ -0,0 +1,138 @@
+package com.example.usans;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    public static final String TAG = "ServerAuthCodeActivity";
+    private static final int RC_GET_AUTH_CODE = 9003;
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private TextView mAuthCodeTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //mAuthCodeTextView = findViewById(R.id.detail);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+
+        validateServerClientID();
+        //OAuth serverClientId
+        updateUI();
+        String serverClientId = getString(R.string.server_client_id);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestServerAuthCode(serverClientId)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void getAuthCode() {
+        //인증 코드가 온다.
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_GET_AUTH_CODE);
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                updateUI();
+            }
+        });
+    }
+
+    private void revokeAccess() {
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI();
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_GET_AUTH_CODE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                String authCode = account.getServerAuthCode();
+
+                String url = "" + authCode;
+
+            } catch (ApiException e) {
+                Log.w(TAG, "Sign-in failed", e);
+                updateUI();
+            }
+        }
+    }
+
+    private void updateUI() {
+        //findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+        //findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+    }
+
+    private void validateServerClientID() {
+        String serverClientId = getString(R.string.server_client_id);
+        String suffix = ".apps.googleusercontent.com";
+        if (!serverClientId.trim().endsWith(suffix)) {
+            String message = "Invalid server client ID in strings.xml, must end with " + suffix;
+
+            Log.w(TAG, message);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                getAuthCode();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+            /*case R.id.disconnect_button:
+                revokeAccess();
+                break;*/
+        }
+    }
+
+}
