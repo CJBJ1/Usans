9457af2069a30d5e9f34093cdaf42d7c40713d75
CJBJ1 <cbjx17@naver.com>
2020-04-18 14:17:04 +0900

    Info 레이아웃,액티비티 추가

---
+++ app/src/main/java/com/example/usans/Info.java
@@ -0,0 +1,20 @@
+package com.example.usans;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Info extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.info_view, container, false);
+
+        return view;
+    }
+}
