fff662db04f06099d72258714ff87a451b98d2ae
Jaehooooooon <jaehoon572@naver.com>
2020-04-27 17:27:17 +0900

    상세화면, 한줄평 작성화면 레이아웃

--- app/src/main/java/com/example/usans/DetailActivity.java
+++ app/src/main/java/com/example/usans/DetailActivity.java
@@ -2,15 +2,31 @@ package com.example.usans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailActivity extends AppCompatActivity {
+    Button writeCommentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        writeCommentButton = findViewById(R.id.write_comment_button);
+        writeCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToWrite();
+            }
+        });
 
+    }
+
+    public void moveToWrite() {
+        Intent intent = new Intent(this, WriteCommentActivity.class);
+        startActivity(intent);
     }
 }
