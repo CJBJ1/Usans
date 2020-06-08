b93cd569b63b2cdff9902becabb420c28a7c2dc9
CJBJ1 <cbjx17@naver.com>
2020-06-08 14:51:00 +0900

    운동영상 데이터 추가

--- app/src/main/java/com/example/usans/Data/HowToMachine.java
+++ app/src/main/java/com/example/usans/Data/HowToMachine.java
@@ -26,6 +26,95 @@ public class HowToMachine {
         url2 = "https://www.youtube.com/watch?v=W2qFdHDskY4";
     }
 
+    public void setLatPullDown(){
+        title1 = "랫풀다운 l 강경원";
+        title2 = "랫풀다운과 풀업의차이(작은 생각의 차이가 운동을 크게 바꿉니다)";
+        thumbnail1 = "https://img.youtube.com/vi/zNgYOHXt-w8/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/kdMF8nJ-lqk/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=zNgYOHXt-w8";
+        url2 = "https://www.youtube.com/watch?v=kdMF8nJ-lqk";
+    }
+
+    public void setBench(){
+        title1 = "완벽한 벤치프레스 강의 [운동의 정석]";
+        title2 = "벤치 프레스 기초 I 강경원";
+        thumbnail1 = "https://img.youtube.com/vi/0DsXTSHo3lU/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/9K81DQFutm8/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=0DsXTSHo3lU";
+        url2 = "https://www.youtube.com/watch?v=9K81DQFutm8";
+    }
+
+    public void setSquat(){
+        title1 = "\'완벽한 스쿼트\' 제대로 하는 방법";
+        title2 = "스쿼트의 모든것(Squat)";
+        thumbnail1 = "https://img.youtube.com/vi/Fk9j6pQ6ej8/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/DqcH7Rq1UMc/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=Fk9j6pQ6ej8";
+        url2 = "https://www.youtube.com/watch?v=DqcH7Rq1UMc";
+    }
+
+    public void setLegExtention(){
+        title1 = "하체운동-레그익스텐션(Leg Extension)의 모든것!";
+        title2 = "레그익스텐션머신의 올바른 운동법 [헬스바이블]Legextention M/C Workout";
+        thumbnail1 = "https://img.youtube.com/vi/rIoPAhtMdvw/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/-RpN9QLJr00/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=rIoPAhtMdvw";
+        url2 = "https://www.youtube.com/watch?v=-RpN9QLJr00";
+    }
+
+    public void setLegCurl(){
+        title1 = "레그컬 이 영상 보고 쉽게 따라해보세요 (레그컬 할 때 허리아픈 분들은 필독!!)";
+        title2 = "하체운동ㅣ허벅지 뒤 운동 레그 컬 초보자들을 위한 올바른 운동법";
+        thumbnail1 = "https://img.youtube.com/vi/u4_3GSIctc4/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/bsqXhhjnwmY/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=u4_3GSIctc4";
+        url2 = "https://www.youtube.com/watch?v=bsqXhhjnwmY";
+    }
+    public void setChestPress(){
+        title1 = "이게 바로 가슴운동 이다!!!(가슴운동은 가슴으로 해라!)";
+        title2 = "1분이면 완벽이해가능!! 체스트프레스 편 (1분OX)";
+        thumbnail1 = "https://img.youtube.com/vi/cAf6I23p_YU/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/1pBcQ-77ho4/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=cAf6I23p_YU";
+        url2 = "https://www.youtube.com/watch?v=1pBcQ-77ho4";
+    }
+
+    public void setButterFly(){
+        title1 = "가슴운동ㅣ넓은 가슴과 안쪽가슴 볼륨감 채우는 체스트 플라이 머신ㅣMSG훈";
+        title2 = "안쪽가슴 채우는 운동의 기본 체스트플라이(팩덱플라이) 이론+영상 깔끔정리";
+        thumbnail1 = "https://img.youtube.com/vi/H-RM4stCBv4/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/ZdJ3a6pQzKE/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=H-RM4stCBv4";
+        url2 = "https://www.youtube.com/watch?v=ZdJ3a6pQzKE";
+    }
+
+    public void setDips(){
+        title1 = "딥스의 정석. 가장 \"완벽한 딥스\" 자세 배우기 교본";
+        title2 = "딥스 \'이렇게\' 제발 하지마세요 (어깨 박살)";
+        thumbnail1 = "https://img.youtube.com/vi/pQSfXvaQGas/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/pqn9koa_wrw/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=pQSfXvaQGas";
+        url2 = "https://www.youtube.com/watch?v=pqn9koa_wrw";
+    }
+
+    public void setShoulderPress(){
+        title1 = "ENG SUB)어깨 벌크업엔 숄더프레스! 어깨 운동 shoulder press";
+        title2 = "숄더프레스머신 기본 총정리편/헬스장 기구운동의 정석/제대로 알려드립니다!";
+        thumbnail1 = "https://img.youtube.com/vi/4i_5I82AowU/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/iatpz9JDb30/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=4i_5I82AowU";
+        url2 = "https://www.youtube.com/watch?v=iatpz9JDb30";
+    }
+
+    public void setArmCurl(){
+        title1 = "이두운동] 이두 빨리 키우는법 암컬머신 배워보자 !!!";
+        title2 = "[이두]바이셉스컬 머신 운동방법 BICEPS CURL MACHINE 암컬머신ARM CURL";
+        thumbnail1 = "https://img.youtube.com/vi/ORW94oG1XRk/0.jpg";
+        thumbnail2 ="https://img.youtube.com/vi/iatpz9JDb30/0.jpg";
+        url1 = "https://www.youtube.com/watch?v=_E9v0cMIgTc";
+        url2 = "https://www.youtube.com/watch?v=ORW94oG1XRk";
+    }
+
     public String getThumbnail1() {
         return thumbnail1;
     }
--- app/src/main/java/com/example/usans/SceneFragment/RoutineDetailFragment.java
+++ app/src/main/java/com/example/usans/SceneFragment/RoutineDetailFragment.java
@@ -53,11 +53,62 @@ public class RoutineDetailFragment extends Fragment {
                 adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
                         howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
             }
-            else{
+            else if(list.get(i).equals("친업")){
                 howToMachine.setChinUp();
                 adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
                         howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
             }
+            else if(list.get(i).equals("랫풀다운머신")){
+                howToMachine.setLatPullDown();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("벤치프레스")){
+                howToMachine.setBench();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("체스트프레스머신")){
+                howToMachine.setChestPress();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("버터플라이머신")){
+                howToMachine.setButterFly();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("딥스")){
+                howToMachine.setDips();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("스쿼트")){
+                howToMachine.setSquat();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("레그익스텐션머신")){
+                howToMachine.setLegExtention();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("레그컬머신")||list.get(i).equals("스탠딩레그컬머신")){
+                howToMachine.setLegCurl();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("숄더프레스머신")){
+                howToMachine.setShoulderPress();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+            else if(list.get(i).equals("암컬머신")){
+                howToMachine.setArmCurl();
+                adapter.addItem(new RoutineDetailItem(i,list.get(i),howToMachine.getThumbnail1(),howToMachine.getTitle1(),howToMachine.getUrl1(),
+                        howToMachine.getThumbnail2(),howToMachine.getTitle2(),howToMachine.getUrl2()));
+            }
+
         }
         return view;
     }
