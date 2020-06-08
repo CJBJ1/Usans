430f3088bfbce60fb8299a2ab2392e798f5e72eb
Jaehooooooon <jaehoon572@naver.com>
2020-06-08 19:03:08 +0900

    댓글 새로고침 추가

--- app/src/main/java/com/example/usans/SceneFragment/BoardDetailFragment.java
+++ app/src/main/java/com/example/usans/SceneFragment/BoardDetailFragment.java
@@ -80,6 +80,10 @@ public class BoardDetailFragment extends Fragment {
                     CommentNetworkTask commentNetworkTask = new CommentNetworkTask(url, null);
                     commentNetworkTask.execute();
                     contentEditText.setText("");
+
+                    String url2 = "http://3.34.18.171:8000/arti/read/?id=";
+                    NetworkTask networkTask = new NetworkTask(url2+titleId, null);
+                    networkTask.execute();
                 } else {
                     AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                     builder.setTitle("로그인이 필요합니다.");
